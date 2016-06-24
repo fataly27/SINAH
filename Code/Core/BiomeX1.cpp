c1f8302b6fac5439da2f0f9dd28c6185c1185823
Hugo <h.guiter@callimedia.fr>
2016-06-24 10:21:31 +0200

    Légère diminution du nombre inutilement grand de terrains à 3 biomes (12->8) et légère augmentation en compensation du nombre de terrains à 1 biome (12->16)

--- Code/Core/BiomeX1.cpp
+++ Code/Core/BiomeX1.cpp
@@ -4,7 +4,7 @@ BiomeX1::BiomeX1(std::vector<int> liste)
 {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::mt19937 engine(seed);
-	std::uniform_int_distribution<int> distribution(1, 3);
+	std::uniform_int_distribution<int> distribution(1, 4);
 	int version = distribution(engine);
 
 	mUrl = "Landscapes/HeightMaps/1Biome/" + std::to_string(liste[0]) + "/" + std::to_string(version) + ".png";
--- Code/Core/BiomeX3.cpp
+++ Code/Core/BiomeX3.cpp
@@ -6,7 +6,7 @@ BiomeX3::BiomeX3(std::vector<int> liste)
 
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::mt19937 engine(seed);
-	std::uniform_int_distribution<int> distribution(1, 3);
+	std::uniform_int_distribution<int> distribution(1, 2);
 	int version = distribution(engine);
 	
 	mUrl = "Landscapes/HeightMaps/1Biome/" + std::to_string(liste[0]) + "-" + std::to_string(liste[1]) + "-" + std::to_string(liste[2]) + "/" + std::to_string(version) + ".png";

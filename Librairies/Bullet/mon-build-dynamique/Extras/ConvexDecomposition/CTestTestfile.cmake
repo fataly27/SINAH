2b1d61f4bd9caccefbab15909ea7cba06d884b74
Hugo <h.guiter@callimedia.fr>
2016-06-24 09:28:15 +0200

    Suppression de dossiers inutiles de Bullet (exemples, extras...)

--- Librairies/Bullet/mon-build-dynamique/Extras/ConvexDecomposition/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/z-Autres/Programmation/Sinah/SINAH/Librairies/Bullet/Extras/ConvexDecomposition
-# Build directory: D:/z-Autres/Programmation/Sinah/SINAH/Librairies/Bullet/mon-build-dynamique/Extras/ConvexDecomposition
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Librairies/Bullet/mon-build-dynamique/Extras/HACD/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/z-Autres/Programmation/Sinah/SINAH/Librairies/Bullet/Extras/HACD
-# Build directory: D:/z-Autres/Programmation/Sinah/SINAH/Librairies/Bullet/mon-build-dynamique/Extras/HACD
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
